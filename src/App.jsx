f9b954ac7c0182f0ff857b050d786be7e1810724
CartezDew <cartez.dewberry@gmail.com>
2025-08-11 16:46:33 -0400

    updated browser title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import PageTitle from './components/PageTitle'
 
 function App() {
   return (
     <div className="App">
+      <PageTitle />
       <Navbar />
       <Home />
     </div>
